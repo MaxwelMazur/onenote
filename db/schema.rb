cf48275a744e8662b1eb981c1538142b3f49896d
MaxwelMazur <maxwel.mazur@hotmail.com>
2021-08-21 04:38:57 -0300

    Creation Of Models Note, Tag, Tags Notes and User Notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_21_055519) do
+ActiveRecord::Schema.define(version: 2021_08_21_065147) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tags_on_user_id"
+  end
+
+  create_table "tags_notes", force: :cascade do |t|
+    t.integer "tag_id", null: false
+    t.integer "note_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["note_id"], name: "index_tags_notes_on_note_id"
+    t.index ["tag_id"], name: "index_tags_notes_on_tag_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +50,19 @@ ActiveRecord::Schema.define(version: 2021_08_21_055519) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "users_notes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "note_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["note_id"], name: "index_users_notes_on_note_id"
+    t.index ["user_id"], name: "index_users_notes_on_user_id"
+  end
+
+  add_foreign_key "notes", "users"
+  add_foreign_key "tags", "users"
+  add_foreign_key "tags_notes", "notes"
+  add_foreign_key "tags_notes", "tags"
+  add_foreign_key "users_notes", "notes"
+  add_foreign_key "users_notes", "users"
 end
