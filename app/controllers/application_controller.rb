0b12118673f08f7b37c714b86b03a4ccaea8b960
MaxwelMazur <maxwel.mazur@hotmail.com>
2021-08-21 19:02:29 -0300

    Creation Of Controllers (Notes, Tags and Users) and set routes fo this controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  include CanCan::ControllerAdditions
+
+  # TODO: remove this line
+  skip_before_action :verify_authenticity_token
 end
